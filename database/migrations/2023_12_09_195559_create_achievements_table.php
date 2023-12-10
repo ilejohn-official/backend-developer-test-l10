4d582572027529e4de5bd74fd3839e77ba957efa
Opeyemi Ilesanmi <opeyemi.ilesanmi@vengreso.com>
2023-12-10 01:47:47 +0100

    make name and identifier unique

--- database/migrations/2023_12_09_195559_create_achievements_table.php
+++ database/migrations/2023_12_09_195559_create_achievements_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('achievements', function (Blueprint $table) {
             $table->id();
             $table->string('identifier')->unique();
-            $table->string('name');
+            $table->string('name')->unique();
             $table->integer('order_position');
             $table->string('type');
             $table->integer('unlock_count');
